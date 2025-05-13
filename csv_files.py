5d5c11f76196b10a1df715f5a9bbf90a03cf42aa
NOTMrdeeP <90851917+NOTMrdeeP@users.noreply.github.com>
2025-05-13 09:45:12 +0200

    class CSVFilehandler

--- csv_files.py
+++ csv_files.py
@@ -1,5 +1,8 @@
 import os
 import csv
+from calendar import error
+
+from urllib3.filepost import writer
 
 
 class StudentRecord:
@@ -19,3 +22,44 @@ class StudentRecord:
             "grade2": self.grade2,
             "grade3": self.grade3
         }
+
+class CSVFileHander:
+    """read/write student records into CSV"""
+    def __init__(self, filename, fieldnames):
+        self.filename = filename
+        self.fieldnames = fieldnames
+
+    def read_all(self):
+        if not os.path.exists(self.filename):
+            return []
+        try:
+            with open(self.filename, 'r', newline='') as f:
+                reader = csv.DictReader(f)
+                return list(reader)
+        except IOError as error:
+            print(f"Error reading file: {error}")
+            return[]
+
+    def write_all(self, records):
+        try:
+            with open(self.filename, "w", newline='') as f:
+                writer = csv.DictWriter(f, fieldnames = self.fieldnames)
+                writer.writeheader()
+                writer.writerows([r.to_dict() for r in records])
+            print(f"Data written to {self.filename}")
+        except IOError as error:
+            print(f"Error writing to file: {error}")
+
+    def append(self, record):
+        file_exists = os.path.exists(self.filename)
+        try:
+            with open(self.filename, 'a', newline='') as f:
+                writer = csv.DictWriter(f, fieldnames = self.fieldnames)
+                if not file_exists:
+                    writer.writeheader()
+                writer.writerow(record.to_dict())
+            print(f"Record added to {self.filename}")
+        except IOError as error:
+            print(f"Could not append to file: {error}")
+
+
